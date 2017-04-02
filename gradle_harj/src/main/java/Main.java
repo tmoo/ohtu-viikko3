ca4640b652b6de16ee69c89b1c8f23546f36c154
Tuomo Lehtonen <tuomo.lehtonen@helsinki.fi>
2017-04-02 19:00:21 +0300

    tehtävä 2

--- gradle_harj/src/main/java/Main.java
+++ gradle_harj/src/main/java/Main.java
@@ -4,7 +4,8 @@ import ohtu.Multiplier;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Multiplier kolme = new Multiplier(3);
+        final int kerrottavaNumero = 3;
+        Multiplier kolme = new Multiplier(kerrottavaNumero);
         System.out.println("Anna luku: ");
         int luku = scanner.nextInt();
 
