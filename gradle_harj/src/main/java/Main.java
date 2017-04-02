88e4d24e733df6878cdb886948bb535bb1869e85
Tuomo Lehtonen <tuomo.lehtonen@helsinki.fi>
2017-04-02 18:26:34 +0300

    tehtava 1

---
+++ gradle_harj/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import ohtu.Multiplier;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Multiplier kolme = new Multiplier(3);
+        System.out.println("Anna luku: ");
+        int luku = scanner.nextInt();
+
+        System.out.println("Luku kertaa kolme on " + kolme.multipliedBy(luku));
+    }
+}
